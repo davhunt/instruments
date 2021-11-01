804a1852f49d5d46c48552dbe6235b2b559de3f0
unknown <saidmf@rpi.edu>
2021-11-01 16:31:29 -0400

    DF creation in survey class

--- scripts/instruments/survey.py
+++ scripts/instruments/survey.py
@@ -1,5 +1,7 @@
 from instruments.subscore import Subscore
 
+import pandas as pd
+
 
 class Survey:
     """Class that utilizes Subscore class to score all available subscores.
@@ -9,9 +11,11 @@ class Survey:
     ----------
     name:   str | None
             String that contains the survey name. If none, no file is written.
-    data:   pandas.Dataframe()
-            Dataframe that contains csv survey data
-    subscores:  dict()
+    file_name:  str
+                Filename that contains csv survey data
+    data:    pd.Dataframe
+             Pandas dataframe that contains csv data. Created from private method _load_data()
+    subscores:  dict() | list() | None
                 Dictionary object containing all subscores and the parameters they go to.
 
                 Ex: 
@@ -23,11 +27,15 @@ class Survey:
                         "select": [3, 4, 6, 10, 14],
                         "type": "avg"
                 }}
+
+                Or list of subscore objects. Alternatively, None if no subscores available.
     
     Private Methods
     ----------
     _filter(self):
-            Filter out non-survey data based on self.name, called in init. 
+            Filter out non-survey data based on self.name, called in init.
+    _load_data(self):
+            Use self.file_name to init pandas dataframe and returns.
 
     Public Methods
     ----------
@@ -38,13 +46,18 @@ class Survey:
             Function to call score() and write subsequent csv to filename.
             Modifies original data in place, and returns modified dataframe. 
     """
-    def __init__(self, name, data, subscores):
+    def __init__(self, name, file_name, subscores):
         self.name = name
-        self.data = data
+        self.file_name = file_name
         self.subscores = subscores
 
-        # Once data is initialized, filter out irrelevant data
+        # Init and filter data
+        self.data = self._load_data()
         self.data = self._filter(self.data.copy(), self.name)
+
+    def _load_data(self):
+        file = pd.read_csv(self.file_name, index_col="record_id")
+        return pd.DataFrame(file)
     
     def _filter(self, data, name):
         # keep only the relevant survey data
