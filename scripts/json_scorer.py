cd00ea6adf95cb7b5a97773ca1f28d862d2ebb10
unknown <saidmf@rpi.edu>
2021-11-01 16:31:47 -0400

    Changed json_scorer to only take in filename

--- scripts/json_scorer.py
+++ scripts/json_scorer.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import json
 from instruments.survey import Survey
 
@@ -13,17 +12,11 @@ Output will be labeled "data/outputs/<survey_name>_data.csv"
 with open('scripts/surveys.json','r') as infile:
     surveys = json.load(infile)
 
-# Get input and output paths
-read_data = surveys["input"]
-
-# Load in file specified from json
-file = pd.read_csv(read_data,index_col="record_id")
-data = pd.DataFrame(file)
-
-# Get all surveys
+# Get file name and survey data
+file_name = surveys["input"]
 surveys_wsub = surveys["surveys"]
 
 # Iterate through survey names and generate data
 for name, subscores in surveys_wsub.items():
-    survey_obj = Survey(name, data, subscores)
+    survey_obj = Survey(name, file_name, subscores)
     handle = survey_obj.score_write("data/outputs/" + name + ".csv")
