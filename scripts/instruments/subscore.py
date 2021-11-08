371fb047bd201e8afe442065caa1a12c69f56d6e
unknown <saidmf@rpi.edu>
2021-11-08 18:36:03 -0500

    Added regex to split rows into unique subsets of ses, event, and runs

--- scripts/instruments/subscore.py
+++ scripts/instruments/subscore.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import re
+
 from instruments.score_type import ScoreType
 
 
@@ -9,7 +12,9 @@ class Subscore:
     Instance Variables
     ----------
     name:   str 
-            String representing subscore name. By default is named "total"
+            String representing survey name.
+    sub_name:   str
+                String representing subscore name
     score_type: String
                 String scoretype that represents a ScoreType value. "Sum" by default. 
     threshold:  float 
@@ -17,18 +22,24 @@ class Subscore:
                 questions must be answered.
     select: list() | None
             List containing questions to calculate. By default is None, which indicates all available questions.
-    reverse_select: list() | None
+    reverse_select: dict() | RevQuestion | None
                     List containing questions which are scored via reverse score. By default is None,
                     which indicates no reverse scored questions.
+
+                    Ex: 
+                    {
+                        select: [1, 2, 3]
+                        max_score: 5
+                    }
     conditional:    dict() | ConditionScore | None
                     Dictionary object containing anticedent, answer, and consequent keys.
                     By default is none, which indicates no conditional questions. 
 
                     Ex: 
                     { 
-                        "anti": 1
-                        "answer": 5
-                        "conseq": [3, 4]
+                        "ante": [perc_complete]
+                        "answer": [< 1.0]
+                        "conseq": [other_subscore]
                      } -> 
                      "If question 1 is answered as 5, score questions 3 & 4"
     custom_score:   str | None
@@ -47,6 +58,9 @@ class Subscore:
     _select_questions(self, data):
             Function to select subset of questions on passed data based on self.select.
             Returns modified dataframe. 
+    _get_unique(self, row):
+            Function to get unique sessions, rows, and events.
+            Returns list of labels.
     _reverse_score(self, data):
             To be implemented
     _score_conditional(self, data):
@@ -69,9 +83,10 @@ class Subscore:
     TIME_LABEL = "timestamp"
     COMP_LABEL = "complete"
 
-    def __init__(self, name="total", score_type="sum", threshold=1.0, select=None,
+    def __init__(self, name, sub_name="total", score_type="sum", threshold=1.0, select=None,
                  reverse_select=None, conditional=None, custom_score=None):
         self.name = name
+        self.sub_name = sub_name
         self.score_type = score_type
         self.threshold = threshold
         self.select = select
@@ -79,11 +94,11 @@ class Subscore:
         self.conditional = conditional
         self.custom_score = custom_score
 
-    def _perc_column(self):
-        return "perc_" + self.name + "_complete"
+    def _perc_column(self, label):
+        return self.name + "_" + self.sub_name + "_" + label + "_perc_complete"
 
-    def _scored_column(self):
-        return self.name + "_scored"
+    def _scored_column(self, label):
+        return self.name + "_" + self.sub_name + "_" + label + "_scored"
 
     def _remove_meta(self, data):
         # Create deep copy to prevent modification in place
@@ -101,7 +116,7 @@ class Subscore:
         # If there is no selection, return data with all questions
         # TODO: DRY!
         if self.select is None:
-            return data.filter(items=list(data.filter(regex=rf"_i._").columns))
+            return data.filter(items=list(data.filter(regex=rf"_i[0-9]+_").columns))
 
         select_columns = []
         for num in self.select:
@@ -111,6 +126,14 @@ class Subscore:
         select_data = data.filter(items=select_columns)
 
         return select_data
+
+    def _get_unique(self, data):
+        # Get column names
+        ind_names = list(data.columns)
+        # Filter unique sessions, runs, and events
+        unique_vals = set(
+            [re.sub(rf"{self.name}_i[0-9]+_", "", ind) for ind in ind_names])
+        return unique_vals
 
     def _score_type(self, row):
         # TODO: Implement conditional and reverse scoring
@@ -139,17 +162,29 @@ class Subscore:
         # Filter based on selected questions
         surv_data = self._select_questions(surv_data)
 
-        # Create a dataframe of percentage complete for each index
-        score = pd.DataFrame(index=surv_data.index, columns=[
-                             self._perc_column(), self._scored_column()])
+        # Create a column for each unique session, row, and event
+        unique_vals = self._get_unique(surv_data)
+        unique_cols = []
+        for val in unique_vals:
+            unique_cols.append(self._perc_column(val))
+            unique_cols.append(self._scored_column(val))
+
+        # Create a dataframe of percentage complete and scored column name
+        score = pd.DataFrame(index=surv_data.index, columns=unique_cols)
 
         for index, row in surv_data.iterrows():
-            # Calculate percentage complete of row and assign to column
-            percentage = self.perc_complete(row)
-            score.loc[index, self._perc_column()] = percentage
-            # Calculate score and assign to column if percentage complete is past threshold
-            score.loc[index, self._scored_column()] = self._score_type(
-                row) if percentage >= self.threshold else -1
+            # Calculate score for every unique group of metadata found
+            for unique in unique_vals:
+                # Create copy to prevent modification in place
+                row_set = row.copy()
+                row_set = row_set.filter(regex=rf"_{unique}")
+
+                # Calculate percentage complete of row and assign to column
+                percentage = self.perc_complete(row_set)
+                score.loc[index, self._perc_column(unique)] = percentage
+                # Calculate score and assign to column if percentage complete is past threshold
+                score.loc[index, self._scored_column(unique)] = self._score_type(
+                    row_set) if percentage >= self.threshold else np.NaN
 
         return score
 
