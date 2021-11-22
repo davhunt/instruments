0a5f9ca720340e877ccd920fa83c616968e944bb
F-said <f.saidmuratov@yahoo.com>
2021-11-22 13:23:12 -0500

    Rewrote json_scorer to get all survey results and concat to OG df

--- scripts/json_scorer.py
+++ scripts/json_scorer.py
@@ -1,5 +1,6 @@
 import json
 from instruments.survey import Survey
+import pandas as pd
 
 """
 Code to load in JSON file and score outlined surveys.
@@ -14,9 +15,13 @@ with open('scripts/surveys.json','r') as infile:
 
 # Get file name and survey data
 file_name = surveys["input"]
+out_path = surveys["output"]
 surveys_wsub = surveys["surveys"]
 
 # Iterate through survey names and generate data
+all_surveys = pd.read_csv(file_name, index_col="record_id")
 for name, subscores in surveys_wsub.items():
     survey_obj = Survey(name, file_name, subscores)
-    handle = survey_obj.score_write("data/outputs/" + name + ".csv")
+    handle = survey_obj.score()
+    all_surveys = pd.concat([all_surveys, handle], axis=1)
+all_surveys.to_csv(out_path + "/output.csv")
