7337cf0a072974312631a1e89ab33b7dea6a3da7
F-said <f.saidmuratov@yahoo.com>
2021-12-06 19:23:26 -0500

    Added input and output arguments

--- scripts/json_scorer.py
+++ scripts/json_scorer.py
@@ -2,6 +2,8 @@ import json
 from instruments.survey import Survey
 import pandas as pd
 
+import sys
+
 """
 Code to load in JSON file and score outlined surveys.
 Simply run and get output! 
@@ -9,19 +11,17 @@ Simply run and get output!
 Output will be labeled "data/outputs/<survey_name>_data.csv"
 """
 
-# Open and save data to dataframe
-with open('scripts/surveys.json','r') as infile:
+input = sys.argv[1]
+out_path = sys.argv[2]
+
+# Open and save survey data
+with open('surveys.json','r') as infile:
     surveys = json.load(infile)
 
-# Get file name and survey data
-file_name = surveys["input"]
-out_path = surveys["output"]
-surveys_wsub = surveys["surveys"]
-
 # Iterate through survey names and generate data
-all_surveys = pd.read_csv(file_name, index_col="record_id")
-for name, subscores in surveys_wsub.items():
-    survey_obj = Survey(name, file_name, subscores)
+all_surveys = pd.read_csv(input, index_col="record_id")
+for name, subscores in surveys.items():
+    survey_obj = Survey(name, input, subscores)
     handle = survey_obj.score()
     all_surveys = pd.concat([all_surveys, handle], axis=1)
 all_surveys.fillna(value="N/A", inplace=True)
