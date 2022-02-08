7279fc72b0e8509b5404170d589995c0a362bc14
F-said <f.saidmuratov@yahoo.com>
2022-02-08 16:16:30 -0500

    Removed test file

--- scripts/json_scorer.py
+++ scripts/json_scorer.py
@@ -39,8 +39,8 @@ def json_score(input_path, survey_dat, output_path=None):
     return all_surveys
 
 if __name__ == "__main__":
-    input =  sys.argv[1]
-    json_data = sys.argv[2]
-    out_path = sys.argv[3]
+    input =  "data\inputs\input-sssq_2022-02-08.csv"
+    json_data = "scripts/surveys.json"
+    out_path = "data/outputs/outputs.csv"
 
     json_score(input, json_data, out_path)
