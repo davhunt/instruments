1cfd0240577e3082ceb653575fb1f89646c2cc37
Farukh <f.saidmuratov@yahoo.com>
2022-07-01 13:54:34 -0400

    Revert "Dev feature appendname"

--- scripts/json_scorer.py
+++ scripts/json_scorer.py
@@ -3,7 +3,6 @@ from survey import Survey
 import pandas as pd
 
 import sys
-import os
 
 
 def json_score(input_path, survey_dat, output_path=None):
@@ -17,7 +16,7 @@ def json_score(input_path, survey_dat, output_path=None):
     survey_dat: str | dict
                 Dictionary or path to json containing survey names and parameters
     output_path:    str | None
-                    string pointing to output path. None if data should not be written out
+                    string pointing to output path and name. None if data should not be written out
     """
     # Open and save survey data
     if (isinstance(survey_dat, str)):
@@ -35,10 +34,8 @@ def json_score(input_path, survey_dat, output_path=None):
         except RuntimeError:
             continue
     all_surveys.fillna(value="NA", inplace=True)
-    if output_path is not None: 
-        # clean up input path for appending by removing path and removing extension
-        handle = os.path.basename(input_path)
-        all_surveys.to_csv(os.path.join(output_path, handle.replace("DATA", "SCRD")))
+    if output_path is not None:  
+        all_surveys.to_csv(output_path)
     return all_surveys
     
 if __name__ == "__main__":
